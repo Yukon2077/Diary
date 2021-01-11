c0e35e500c9ace184280cd37ce251f1dd0917564
Yukon2077 <yuvankumar2077@gmail.com>
2021-01-11 19:36:58 +0530

    Added Settings Activity

---
+++ app/src/main/java/com/yukon/diary/MainActivity.java
@@ -0,0 +1,21 @@
+package com.yukon.diary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void gotoSettings(View view) {
+        Intent settingsIntent = new Intent(this,SettingsActivity.class);
+        startActivity(settingsIntent);
+    }
+
+}
