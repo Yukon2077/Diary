164a4f1d9f88d04c3ce1096591c7cbc0a86b5eda
Yukon2077 <yuvankumar2077@gmail.com>
2021-02-14 11:02:32 +0530

    added back function to settings page

--- app/src/main/java/diary/activity/SettingsActivity.java
+++ app/src/main/java/diary/activity/SettingsActivity.java
@@ -1,6 +1,7 @@
 package diary.activity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -31,5 +32,9 @@ public class SettingsActivity extends AppCompatActivity {
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
         }
+    }
+
+    public void back(View view){
+        finish();
     }
 }
